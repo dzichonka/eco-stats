289b3152c625b6062081c29d2d4de1f4b49a08ad
Anna Vasilevich <dzichonka@gmail.com>
2025-08-26 23:03:18 +0200

    feat: add table for data display

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,9 +4,12 @@ import Loader from '../Loader/Loader';
 
 function App() {
   return (
-    <Suspense fallback={<Loader />}>
-      <EmissionsList />
-    </Suspense>
+    <>
+      <h1 className="text-center text-2xl">Eco Stats App</h1>
+      <Suspense fallback={<Loader />}>
+        <EmissionsList />
+      </Suspense>
+    </>
   );
 }
 
--- src/components/EmissionsTable/EmissionsTable.tsx
+++ src/components/EmissionsTable/EmissionsTable.tsx
@@ -9,11 +9,33 @@ const EmissionsTable = ({ data }: EmissionsTableProps) => {
   const arr = Object.entries(data);
   const deferredData = useDeferredValue(arr);
   return (
-    <ul>
-      {deferredData.map(([country, countryData]) => (
-        <li key={countryData.iso_code}>{country}</li>
-      ))}
-    </ul>
+    <table className="relative">
+      <caption>caption</caption>
+      <thead className="bg-black h-12 sticky top-0">
+        <tr>
+          <th>country</th>
+          <th>iso_code</th>
+          <th>year</th>
+          <th>population</th>
+          <th>co2</th>
+          <th>co2_per_capita</th>
+        </tr>
+      </thead>
+      <tbody>
+        {deferredData.map(([country, countryData]) => (
+          <tr key={country}>
+            <td>{country}</td>
+            <td>{countryData.iso_code}</td>
+            <td>{countryData.data[countryData.data.length - 1].year}</td>
+            <td>{countryData.data[countryData.data.length - 1].population}</td>
+            <td>{countryData.data[countryData.data.length - 1].co2}</td>
+            <td>
+              {countryData.data[countryData.data.length - 1].co2_per_capita}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
